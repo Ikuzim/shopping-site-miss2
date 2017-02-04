33c514e4af6afd6424cebc252669fa06b53fcdbf
mizukishimada <smk72ikuzim32volare@icloud.com>
2017-02-04 16:51:28 +0000

    shopping-site

---
+++ shopping-site/js/linkmenu.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $("#PAGE_LINK h4").click(function () { 
+    if($(this).hasClass("openlink")){ 
+      $(this).removeClass("openlink"); 
+    }else{
+      $(this).addClass("openlink"); 
+    }
+      $("#PAGE_LINK h4 + ul").slideToggle(); 
+  });
+
+  $("#CATEGORIES_LINK h4").click(function () { 
+    if($(this).hasClass("openlink")){
+        $(this).removeClass("openlink"); 
+    }else{
+      $(this).addClass("openlink");
+    }
+      $("#CATEGORIES_LINK h4 + ul").slideToggle(); 
+  });
+});
---
+++ shopping-site/js/spmenu.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+  $("#SP_MENU_BUTTON").click(function () {
+    $("#PC_MENU").slideToggle();
+  });
+});
